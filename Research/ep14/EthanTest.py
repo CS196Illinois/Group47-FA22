c578c145854ccaf0d7d4b8387c2f1ee6e206a8f8
ethanccpeng <112594087+ethanccpeng@users.noreply.github.com>
2022-10-06 22:57:17 -0500

    Ingredients Only

--- Research/ep14/EthanTest.py
+++ Research/ep14/EthanTest.py
@@ -4,5 +4,31 @@ import pytesseract
 
 # use easyocr to read the image
 reader = easyocr.Reader(['en'])
-result = reader.readtext('EthanTest.png')
-print(result)
+
+# Problems with code: Doesn't filter out text after Ingredients: keyword has been found
+# Possible solution: look for a period and once found, stop returning words
+# Problem with solution: easyocr is not identifying the periods
+
+#label = reader.readtext("/content/ingredients label 1.png")
+label = reader.readtext("EthanTest.png")
+print(label)
+# Construct a list of strings containing all the words on the label 
+wordBlocks = []
+for element in label: # Populate list
+  wordBlocks.append(element[1])
+print("---------------------") # delimiter
+
+ingredientsList = [] # List of ingredients
+
+afterIngredients = False
+for wordBlock in wordBlocks: # Searching for the "Ingredients:"
+  if ":" in wordBlock:
+    afterIngredients = True
+  if afterIngredients: # Print/add remaining words (any words that appear physically after Ingredients:)
+    # print(wordBlock)
+    ingredientsList.append(wordBlock)
+
+print(ingredientsList)
+ingredientsString = ""
+ingredientsString = ingredientsString.join(ingredientsList) # Creating a joint string to get rid of the awkward "word blocks"
+print(ingredientsString)
