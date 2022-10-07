c8c44531161981a40fd9ca0a4e5b8af9eb3e7246
ethanccpeng <112594087+ethanccpeng@users.noreply.github.com>
2022-10-06 22:43:27 -0500

    Py Tesseract and OpenCV

--- Research/ep14/EthanTest.py
+++ Research/ep14/EthanTest.py
@@ -1,3 +1,12 @@
 import easyocr
+import cv2
+
+# Read text from an image 
 reader = easyocr.Reader(['en'])
-print("hello")
+results = reader.readtext('~/CS124 Project/Group47-FA22/Research/ep14/EthanTest.png')
+print(results)
+print(results[0])
+print(results[0][1])
+print("----------------")
+for element in results:
+  print(element[1])
