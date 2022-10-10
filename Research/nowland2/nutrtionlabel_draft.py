f1a289e69b9f7580bc2080ef0559b2c9188f4df8
Banks-N <banksnowland@gmail.com>
2022-10-10 03:15:18 -0500

    Sprint 2

--- Research/nowland2/nutrtionlabel_draft.py
+++ Research/nowland2/nutrtionlabel_draft.py
@@ -7,52 +7,99 @@ Original file is located at
     https://colab.research.google.com/drive/14Fbk1Yz-l1cJbyOHyPHyQKO8587Nvr1z
 """
 
+#Working Python 3.9.6
+
+import re #pip install regex
 import easyocr
 import cv2
 
-img = cv2.imread('/content/nutritionlabel.png')
-img1 = cv2.imread('/content/nutritionlabel1.png')
+
+img = cv2.imread(r"Research\nowland2\content\nutritionlabel.png")
+# img1 = cv2.imread(r'Research\nowland2\content\nutritionlabel.png')
 reader = easyocr.Reader(['en'])
 
 results = reader.readtext(img)
-results1 = reader.readtext(img1)
+# results1 = reader.readtext(img1)
 
 #for element in results:
   #print(element[1])
 
+# Worse Version (picture doesn't work too well with Computer Vision)
+
+# Ingredients = False
+
+# for element in results:
+#   if element[2] > 0:
+
+#     if ":" in element[1]:
+#       Ingredients = True
+#     if Ingredients:
+#       if "," in element[1]:
+#         split = element[1].split(",")
+#         for word in split:
+#           if word != "":
+#             print(word)
+#       else:
+#         print(element[1])
+#         #print(element[2])
+
+# results = results1
+
 Ingredients = False
 
-for element in results:
-  if element[2] > 0:
+def filteredPrint(str) :
+  """
+  This function returns the original string with the following edits: 
+   - all lowercase 
+   - no hyphens 
+   - no trailing and leading spaces
+   - autocorrected with "speller" from the "autocorrect" library
+  """
 
-    if ":" in element[1]:
+  #pip install autocorrect
+  from autocorrect import Speller 
+
+
+  spell = Speller(only_replacements=True) #Initalize spellecheck, with settings for correcting computer vision
+
+  # print(str) #Print orignal String
+
+  str = str.lower() #lowercase
+  str = str.lstrip().rstrip() #remove trailing and leading white space
+  prefilter = spell(str) #spell check 
+
+  #remove special characters except ' ' and  '-'
+  remove = """!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+  remove = remove.replace("-", "") # don't remove hyphens
+  pattern = r"[{}]".format(remove) # create the pattern
+  filtered = re.sub(pattern, "", prefilter)
+
+  print(filtered)
+  return filtered
+
+
+print("--------------------------------------------------")
+
+for element in results:
+
+  if element[2] > 0:
+    ingredient = element[1].lower()
+
+    if "ingre" in ingredient:
       Ingredients = True
     if Ingredients:
-      if "," in element[1]:
-        split = element[1].split(",")
+
+      if "," in ingredient:
+        split = ingredient.split(",")
         for word in split:
           if word != "":
-            print(word)
-      else:
-        print(element[1])
-        #print(element[2])
-
-results = results1
-
-Ingredients = False
-
-for element in results:
-  if element[2] > 0:
-
-    if "INGRE" in element[1]:
-      Ingredients = True
-    if Ingredients:
-      if "," in element[1]:
-        split = element[1].split(",")
+            filteredPrint(word)
+      elif "and" in ingredient:
+        split = ingredient.split("and")
         for word in split:
           if word != "":
-            print(word)
+            filteredPrint(word)
       else:
-        print(element[1])
+        filteredPrint(ingredient)
         #print(element[2])
 
