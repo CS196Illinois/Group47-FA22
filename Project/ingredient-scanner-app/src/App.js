da983ce8d306ff4b477fd8e38d8cacd63a8ae9c2
ethanccpeng <112594087+ethanccpeng@users.noreply.github.com>
2022-10-17 17:19:31 -0500

    Added Title

--- Project/ingredient-scanner-app/src/App.js
+++ Project/ingredient-scanner-app/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Title from '../src/Components/Title.jsx';
 
 function App() {
   return (
     <div className="App">
-      
+      <Title> </Title>
     </div>
   );
 }
---
+++ Project/ingredient-scanner-app/src/Components/Title.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Title() {
+  return (
+    <div>
+        <h1> Ingredient Scanner </h1>
+    </div>
+  )
+}
