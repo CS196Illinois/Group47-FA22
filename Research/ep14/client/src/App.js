a84ab3f2d25399b98c02cd65cb7eda3868038edd
ethanccpeng <112594087+ethanccpeng@users.noreply.github.com>
2022-11-06 12:23:02 -0600

    Flask tutorial

---
+++ Research/ep14/client/src/App.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from 'react'
+
+function App() {
+console.log("cum")
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+    fetch("http://localhost:5000/members", 'no-cors').then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, []) // <-- empty array means run once
+
+  return (
+    <div>
+
+        {(typeof data.members === 'undefined') ? ( // if members is not fetched yet
+          <p>loading...</p>
+        ) : (
+          data.members.map((member, i) => (
+            <p key={i}>{member}</p>
+          ))
+        )}
+
+    </div>
+  )
+}
+
+export default App
