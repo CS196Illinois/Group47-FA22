ef5fa2a90bf1245acc09eed90451c4100448a267
Banks-N <banksnowland@gmail.com>
2022-11-09 21:07:06 -0600

    Tutorial attempt

---
+++ Research/nowland2/banks-react-flask/flask-server/server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app)
+# CORS(app)
+
+# Members API  Route
+@app.route("/members")
+def members():
+    return {"members" : ["Member1", "Member 2", "Member 3"]}
+    # return 1
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
