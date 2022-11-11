c864c17d1ed383cdea7c8ce42801b2a72ef85ad1
tsurya89 <thea.surya@gmail.com>
2022-11-11 16:59:03 -0600

    fixed flask tutorial code

--- Project/theas-flask-tutorial/flask-server/client/src/App.js
+++ Project/theas-flask-tutorial/flask-server/client/src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
 
+
 export default function App() {
   const [data, setData] = useState([{}])
   useEffect(() => {
-    fetch("/members").then(
+    fetch("http://127.0.0.1:5000/members").then(
       res => res.json()
     ).then(
         data => {
--- Project/theas-flask-tutorial/flask-server/server.py
+++ Project/theas-flask-tutorial/flask-server/server.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask;
+from flask_cors import CORS;
 
 app = Flask(__name__)
+
+CORS(app)
 
 # Members API route
 
@@ -8,5 +11,11 @@ app = Flask(__name__)
 def members():
     return {"members": ["Member1", "Member2", "Member3"]}
 
+
+
+@app.route("/")
+def index():
+    return {"thea": ["thea1", "thea2", "thea3"]}
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
