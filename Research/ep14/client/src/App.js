4925c5f0f4c523ff5db7ee98544741fa4119ec24
ethanccpeng <112594087+ethanccpeng@users.noreply.github.com>
2022-11-28 17:08:42 -0600

    POST JSON request works, image does not

--- Research/ep14/client/src/App.js
+++ Research/ep14/client/src/App.js
@@ -1,4 +1,8 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import photo from './desmos.png'
+import FormData from 'form-data'
+
 
 function App() {
   const [data, setData] = useState([{}])
@@ -15,11 +19,50 @@ function App() {
     )
   }, []) // <-- empty array means run once
 
+  let bodyFormData = new FormData();
+  bodyFormData.append('image', photo)
+
+
+  // const dt = {'ingredients': "Apples" };
+  // axios.post("http://127.0.0.1:8000/ingredients", dt)
+  const handleClick1 = async() => {
+    const {data} = await axios({
+      method: "post",
+      url: "http://127.0.0.1:8000/ingredients",
+      //data: bodyFormData,
+      data: {'ingredients': "Peaches"},
+      contentType: "application/json"
+    })
+    .then(function (response) {
+      console.log(response)
+    })
+    .then(function (error) {
+      console.log(error)
+    });
+  }
+  
+
+  var files = {'file': ('photoUpload', photo)}
+
+  const handleClick2 = async() => {
+    const {data} = await axios({
+      method: "post",
+      url: "http://127.0.0.1:8000/scan",
+      data: files,
+      contentType: 'multipart/form-data'
+    })
+    .then(function (response) {
+      console.log(response)
+    })
+    .then(function (error) {
+      console.log(error)
+    });
+  }
+
   return (
     <div>
-      {console.log("Looking for members")}
-      {/* {(typeof data.members === 'undefined') ? (console.log("No members")) : (console.log("Members found"))} */}
-
+      <button onClick={handleClick1}>Make ingredients post request</button>
+      <button onClick={handleClick2}>Make photo post request</button>
       {(typeof data.members === 'undefined') ? ( // if members is not fetched yet
         <p>loado...</p>
       ) : (
@@ -27,7 +70,6 @@ function App() {
           <p key={i}>{member}</p>
         ))
       )}
-      {console.log("end")}
     </div>
   )
 }
