dfc2ddf0523ecc5af8a9ded855f3f02dcef10163
Banks <banksnowland@gmail.com>
2022-11-28 17:39:19 -0600

    Not working venv

---
+++ Project/venv/server.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_cors import CORS
+from scanner import Scanner
+
+app = Flask(__name__)
+# cors = CORS(app)
+CORS(app)
+
+# Members API  Route
+@app.route("/members")
+def members():
+    return {"members" : ["Member1", "Member 2", "Member 3"]}
+    # return 1
+
+# Members API  Route
+@app.route("/scanner")
+def scanner():
+    scanner = Scanner()
+    return scanner.testImage()
+
+if __name__ == "__main__":
+    app.run(debug=True)
