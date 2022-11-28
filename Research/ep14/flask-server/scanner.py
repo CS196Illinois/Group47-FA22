b453b6599a02fb22df616d0cf985ec86d7dce7a7
ethanccpeng <112594087+ethanccpeng@users.noreply.github.com>
2022-11-28 17:16:09 -0600

    Json post request ok, image post request not ready

--- Research/ep14/flask-server/scanner.py
+++ Research/ep14/flask-server/scanner.py
@@ -1,6 +1,50 @@
+import cv2
+import pytesseract
+import os
+
 class Scanner:
-    def scan(self): 
-        ingredients = ""
-        for i in range(10):
-            ingredients += "ingredient" + str(i)
-        return ingredients
+    def scan(self, path): 
+        # use opencv to read the text from the image
+        print("Image Path is " + path)
+        photo = path
+        defaultfilepath = 'desmos_copy.png'
+        image = cv2.imread(path)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        rawtext = pytesseract.image_to_string(gray, lang="eng").lower()
+
+        # isolate the ingredients section
+        colonIndex = rawtext.find(":")
+        periodIndex = rawtext[colonIndex:].find(".")
+        print(rawtext)
+        ingredientsOnly = rawtext[colonIndex+2:colonIndex+periodIndex]
+        print(ingredientsOnly)
+
+        #remove filler words/symbols
+        #ingredientsOnly = ingredientsOnly.replace("*", "")
+        ingredientsOnly = ingredientsOnly.replace("\n", " ")
+        ingredientsOnly = ingredientsOnly.replace("™", "")
+        ingredientsOnly = ingredientsOnly.replace(", and ", ",")
+        ingredientsOnly = ingredientsOnly.replace(" and ", ",")
+
+        #split by commas
+        ingredientsList = ingredientsOnly.split(",")
+
+        #remove leading and trailing whitespace from each ingredient
+        ingredientsList = [ingredient.strip() for ingredient in ingredientsList]
+
+        #Ingredient end check:
+        #remove any asterisked statements (not real ingredients)
+        lastCutoff = -1
+        for index, ingredient in enumerate(ingredientsList):
+            cutoff = ingredient.find("*")
+            if (cutoff != -1):
+                ingredientsList[index] = ingredient[:cutoff]
+                lastCutoff = index
+            
+        #find final item in List with a asterisk and delete "ingredients" after it
+        if (lastCutoff != -1):
+            del ingredientsList[lastCutoff+1:]
+        return ingredientsList
+        #for ingredient in ingredientsList:
+            #match each ingredient to the database
+            #print("Ingredient: " + ingredient)
--- Research/ep14/flask-server/server.py
+++ Research/ep14/flask-server/server.py
@@ -1,27 +1,69 @@
-from flask import Flask
+import os
+from flask import Flask, redirect, url_for, request, flash
 from flask_cors import CORS
 from scanner import Scanner
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = './'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 CORS(app)
+
+ingredientsList = {"ingredients": "Apples"}
+savedPhoto = {"file": ""}
+
+def allowed_file(filename):
+    return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # Members API Route
 @app.route('/members')
 def members():
-    return {"members": ["Member10", "Member2", "Member3"]}
+    return {"members": ["Member10", "Member2", "Member4"]}
 
 @app.route('/')
 def index():
     return {"hi": "ethan"}
 
-@app.route('/scan')
-# define a funnction that takes in a picutre, converts it to a string, and calls the api function to return the nutrition facts
+@app.route('/ingredients', methods = ['POST', 'GET'])
+def ingredients():
+    # if request.method == 'POST':
+    if request.method == "POST":
+        print(request.data)
+        print(request.json)
+        print(request.json['ingredients'])
+        ingredientsList['ingredients'] = request.json['ingredients']
+        return request.json
+    else:
+        return ingredientsList
+
+@app.route('/photo', methods = ['POST', 'GET'])
+def photo():
+    if request.method == 'POST':
+        print(request.data)
+        savedPhoto['file'] = request.data
+        return request.data
+    else:
+        return savedPhoto
+
+@app.route('/scan', methods = ['POST', 'GET'])
+# define a funnction that takes in a picture, converts it to a string, and calls the api function to return the nutrition facts
 # api function: takes in a string of converted text, fetches it info from the database, and returns the nutrition facts
 # construct a json object with the nutrition facts
 # have front end fetch the json object and display the nutrition facts
 def scan():
     scanner = Scanner()
-    return {"ingredients": scanner.scan()}
+    if request.method == 'POST':
+        print(request.data)
+        print(request.json)
+        print(request.json['file'])
+        #print(scanner.scan(request.json['file']))
+        return request.json['file']
+    else:
+        return {"hi": "ethan"}
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
